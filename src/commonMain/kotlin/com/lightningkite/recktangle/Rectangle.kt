b5f9d129c6be6dbf8b80692e863d2c4f2b703ad3
Joseph Ivie <josephivie@gmail.com>
2018-11-08 14:56:26 -0700

    Fix to observing last item

--- src/commonMain/kotlin/com/lightningkite/recktangle/Rectangle.kt
+++
@@ -1,8 +0,0 @@
-package com.lightningkite.recktangle
-
-class Rectangle(
-        var left: Float = 0f,
-        var top: Float = 0f,
-        var right: Float = 0f,
-        var bottom: Float = 0f
-)
--- src/commonMain/kotlin/com/lightningkite/recktangle/squared.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
-package com.lightningkite.recktangle
-
-
-inline fun Float.squared(): Float = this * this
-inline fun Double.squared(): Double = this * this
