61d799fbcb6348a3d40b74a738366579e20156a8
Joseph Ivie <josephivie@gmail.com>
2019-03-27 18:30:01 -0600

    Ideas for multiplatform layout system

--- src/commonMain/kotlin/com/lightningkite/recktangle/Rectangle.kt
+++ src/commonMain/kotlin/com/lightningkite/recktangle/Rectangle.kt
@@ -5,4 +5,51 @@ class Rectangle(
         var top: Float = 0f,
         var right: Float = 0f,
         var bottom: Float = 0f
-)
+) {
+    var centerX: Float
+        get() = (left + right) / 2
+        set(value) {
+            val delta = value - (left + right) / 2
+            left += delta
+            right += delta
+        }
+    var centerY: Float
+        get() = (top + bottom) / 2
+        set(value) {
+            val delta = value - (top + bottom) / 2
+            top += delta
+            bottom += delta
+        }
+    val width: Float
+        get() = (right - left)
+
+    fun setWidthFromCenter(value: Float) {
+        val oldWidth = width
+        val delta = value - oldWidth
+        left -= delta * .5f
+        right += delta * .5f
+    }
+
+    val height: Float
+        get() = (bottom - top)
+
+    fun setHeightFromCenter(value: Float) {
+        val oldHeight = height
+        val delta = value - oldHeight
+        top -= delta * .5f
+        bottom += delta * .5f
+    }
+
+    fun normalize() {
+        if (left > right) {
+            val temp = right
+            right = left
+            left = temp
+        }
+        if (top > bottom) {
+            val temp = bottom
+            bottom = top
+            top = temp
+        }
+    }
+}
